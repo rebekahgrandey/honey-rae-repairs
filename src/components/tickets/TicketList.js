d18a568879a840ec55541332ca2de819844c3f46
Rebekah Taylor <rebekahtdesigns@gmail.com>
2022-10-11 15:45:05 -0500

    add main

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,13 +1,50 @@
 import { useEffect, useState } from "react"
+import "./Tickets.css"
 
 export const TicketList = () => {
     const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFiltered] = useState([])
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
 
     useEffect(
         () => {
-            console.log("Initial state of tickets", tickets) // View the initial state of tickets
+            fetch(`http://localhost:8088/serviceTickets`)
+                .then(response => response.json())
+                .then((ticketArray) => {
+                    setTickets(ticketArray)
+
+                })
         },
         [] // When this array is empty, you are observing initial component state
     )
-    return <h2>List of Tickets</h2>
+
+    useEffect(
+        () => {
+            if (honeyUserObject.staff){
+                setFiltered(tickets)
+            }
+            else {
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }
+        }, [tickets]
+    )
+    return <>
+        <h2>List of Tickets</h2>
+
+        <article className="tickets">
+            {
+                filteredTickets.map(
+                    (ticket) => {
+                        return <section className="ticket">
+                            <header>{ticket.description}</header>
+                            <footer>Emergency: {ticket.emergency ? "❗️" : "No"}</footer>
+                        </section>
+                    }
+                )
+            }
+        </article>
+    </>
 }
